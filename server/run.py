dca6660ca376e2ce777dbf59e287a3be3b82b68a
Henri Hulski <henri.hulski@gazeta.pl>
2017-01-15 02:32:45 +0100

    fix: repeated database binding

--- server/run.py
+++ server/run.py
@@ -27,8 +27,12 @@ def setup_db(app):
     provider = app.settings.database.provider
     args = app.settings.database.args
     kwargs = app.settings.database.kwargs
-    db.bind(provider, *args, **kwargs)
-    db.generate_mapping(create_tables=True)
+    try:
+        db.bind(provider, *args, **kwargs)
+    except TypeError:
+        pass
+    else:
+        db.generate_mapping(create_tables=True)
     add_admin()
 
 
