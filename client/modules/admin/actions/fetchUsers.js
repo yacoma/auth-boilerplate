40bb226b697f39f25a57bb60f3995fa33d9d8391
Henri Hulski <henri.hulski@gazeta.pl>
2017-04-20 17:16:06 +0200

    refactor(client): switch to Array syntax as runSignal does not work with sequence()

--- client/modules/admin/actions/fetchUsers.js
+++ client/modules/admin/actions/fetchUsers.js
@@ -1,12 +1,11 @@
-import {sequence} from 'cerebral'
 import {state, string, props} from 'cerebral/tags'
 import {set, when} from 'cerebral/operators'
 import {httpGet} from 'cerebral-provider-http/operators'
 import showFlash from '../../common/factories/showFlash'
 import mergeUsers from './mergeUsers'
 
-export default sequence('Fetch users from database', [
-  when(state`admin.usersSortDir`, (sortDir) => sortDir === 'ascending'), {
+export default [
+  when(state`admin.usersSortDir`, (usersSortDir) => usersSortDir === 'ascending'), {
     true: set(props`sortDir`, 'asc'),
     false: set(props`sortDir`, 'desc')
   },
@@ -29,4 +28,4 @@ export default sequence('Fetch users from database', [
     ],
     error: showFlash('Could not fetch users from database', 'error')
   }
-])
+]
