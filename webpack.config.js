6fd77c772f924f2cd9918c6e5b8f13829162eaa3
Henri Hulski <henri.hulski@gazeta.pl>
2018-03-28 16:21:46 +0200

    chore(client): upgrade cerebral, xhr-mock, webpack and other dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,15 @@
 const path = require('path');
-const webpack = require('webpack');
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    }
-  })
-];
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        minimize: true,
-        debug: false,
-        context: __dirname
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        unused: true,
-        dead_code: true,
-        warnings: false
-      }
-    })
-  );
-}
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: path.resolve('client', 'index.js'),
+  mode,
+  entry: './client/index.js',
   devtool: 'source-map',
   output: {
+    filename: 'auth.js',
     path: path.resolve('static'),
-    publicPath: '/static/',
-    filename: 'auth.js'
-  },
-  resolve: {
-    modules: [
-      path.resolve('client'),
-      'node_modules',
-    ],
-    extensions: ['.js', '.jsx'],
+    publicPath: '/static/'
   },
   module: {
     rules: [{
@@ -68,6 +35,5 @@ module.exports = {
         presets: [['env', {modules: false}], 'react']
       }
     }]
-  },
-  plugins: plugins
+  }
 };
