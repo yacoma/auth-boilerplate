31e5d5491144b5a524241eb9883657d5ee4dbc13
Henri Hulski <henri.hulski@gazeta.pl>
2020-05-03 16:13:05 +0200

    fix(deploy): update gunicorn config

--- deploy/conf/web/gunicorn.conf.py
+++ deploy/conf/web/gunicorn.conf.py
@@ -1,3 +1,5 @@
-bind = "188.165.237.135:9005"
-workers = 10
+import multiprocessing
+
+bind = "unix:/tmp/proxy_auth-boilerplate.yacoma.it.sock"
+workers = multiprocessing.cpu_count() * 2 + 1
 forwarded_allow_ips = "127.0.0.1, 188.165.237.135"
